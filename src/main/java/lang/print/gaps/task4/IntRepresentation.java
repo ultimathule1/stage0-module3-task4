ec86d8ec5a6bbf590ad6018c84072cc1ec77b85e
Madara <ranel28021998@gmail.com>
2022-12-21 14:45:15 +0300

    It's done

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,6 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        System.out.println((char)99 + (char)104 + (char)97 + (char)114);
     }
 }
